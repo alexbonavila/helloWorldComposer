d3c235de61437c0e0f8424af0451fa8feee45e6f
alexbonavila <alexbonavila@iesebre.com>
2015-10-06 19:41:46 +0200

    hello wordl primer commit

---
+++ Hello.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alumne
+ * Date: 6/10/15
+ * Time: 19:31
+ */
+class Hello
+{
+    public function say_hello(){
+        echo "Hola Mon";
+    }
+}
---
+++ Main.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__ . "/Hello.php";
+$greatings =new Hello();
+
+$greatings ->say_hello();
